e8ba2ddc2d5ecfacbea1f35b61340f62cb256b05
liuyanglong <1261440059@qq.com>
2020-12-25 20:40:01 +0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Rating  from './components/Rating'
 function App() {
   return (
     <div className="App">
-      <h1>Movie Club</h1>
+      <h1>Movie Club 123</h1>
       <Rating score={6.6} />
     </div>
   );
